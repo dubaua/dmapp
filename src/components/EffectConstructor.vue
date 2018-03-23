eed1779843447935fefeefcda6357ce0b0e0cc9b
dubaua <dubaua@gmail.com>
2018-03-23 17:56:55 +0500

    rework markup, rename keys

--- src/components/EffectConstructor.vue
+++ src/components/EffectConstructor.vue
@@ -1,54 +1,59 @@
 <template lang="pug">
-  .effect-constructor
+  //- ec stands for effect constructor
+  .ec
     h1 Effect constructor
-    .container
-      .row.middle-xs
-        .col-xs-3
-          el-radio-group(v-model="chosen_type")
-            el-radio-button(:label="0") Save ends
-            el-radio-button(:label="1") Temporary
-            el-radio-button(:label="2") Conditional
-        template(v-if="chosen_type === 0 || chosen_type === 2")
-          .col-xs-2 Ongoing damage
-          .col-xs-1
-            drag-adjust(
-              v-model="ongoingDamage",
-              :min="0",
-              :max="25")
-              el-input(v-model="ongoingDamage")
-          .col-xs-2 Regeneration
-          .col-xs-1
-            drag-adjust(
-              v-model="regeneration",
-              :min="0",
-              :max="25")
-              el-input(v-model="regeneration")
-        .col-xs-3(v-if="chosen_type === 0")
-          el-button Add Fail Effect
-          el-button Add Aftereffect
-        .col-xs-6(v-if="chosen_type === 1")
-          | Until the
-          el-radio-group(v-model="untilTheEndOfTurn")
-            el-radio-button(:label="true") end
-            el-radio-button(:label="false") start
-          | of
-          el-radio-group(v-model="untilTheTargetsTurn")
-            el-radio-button(:label="true") target's
-            el-radio-button(:label="false") caster's
-          | turn
-        .col-xs-3(v-if="chosen_type === 2")
-          el-input(v-model="endCondition", placeholder="Describe the end condition")
-    p
-      el-input(v-model="effectText", placeholder="Effect text")
+    .ec__row
+      .ec__row
+        .ec__label Describe how effect ends
+        el-radio-group(v-model="chosenType")
+          el-radio-button(:label="0") Save ends
+          el-radio-button(:label="1") Temporary
+          el-radio-button(:label="2") Conditional
+      .ec__row(
+        v-if="chosenType === types.SAVE_ENDS || chosenType === types.CONDITIONAL")
+        .ec__label Ongoing
+        drag-adjust(
+          v-model="ongoingDamage",
+          :min="0",
+          :max="25")
+          el-input(v-model="ongoingDamage")
+        .ec__label damage
+      .ec__row(
+        v-if="chosenType === types.SAVE_ENDS || chosenType === types.CONDITIONAL")
+        .ec__label Regeneration of
+        drag-adjust(
+          v-model="regeneration",
+          :min="0",
+          :max="25")
+          el-input(v-model="regeneration")
+      .ec__row(v-if="chosenType === types.SAVE_ENDS")
+        el-button(type="text") Add Fail Effect
+        el-button(type="text") Add Aftereffect
+      .ec__row(v-if="chosenType === types.TEMPORARY")
+        .ec__label Lasts until
+        el-radio-group(v-model="untilTheEndOfTurn")
+          el-radio-button(:label="true") the end
+          el-radio-button(:label="false") the start
+        .ec__label of
+        el-radio-group(v-model="untilTheTargetsTurn")
+          el-radio-button(:label="true") the target's
+          el-radio-button(:label="false") the caster's
+        .ec__label turn
+      .ec__row(v-if="chosenType === types.CONDITIONAL")
+        el-input(
+          v-model="endCondition",
+          placeholder="Describe the end condition",
+          class="ec__text-input")
+    el-input(v-model="effectText", placeholder="Effect text")
 
     el-checkbox-group(
       v-model="followingConditionsIds",
       class="el-checkbox-group--flex",
       )
-      el-checkbox-button(
+      el-checkbox(
         v-for="condition in conditions",
         :label="condition.id",
-        class="el-checkbox-button--grow",
+        border,
         :key="condition.id") {{condition.title}}
 
     p Modify defenses
@@ -56,30 +61,20 @@
       v-model="defenseModifier",
       :min="-10",
       :max="10")
-      el-input(v-model="defenseModifier")
+      el-input(v-model="defenseModifier", :disabled="isDefenseModifierDisabled")
     el-checkbox(
-      v-model="affectDefence.ac",
+      v-for="(value, key, index) in affectedDefenses",
+      v-model="affectedDefenses[key]",
       @change="handleCheckDefense",
-      border) AC
-    el-checkbox(
-      v-model="affectDefence.fort",
-      @change="handleCheckDefense",
-      border) Fort
-    el-checkbox(
-      v-model="affectDefence.ref",
-      @change="handleCheckDefense",
-      border) Ref
-    el-checkbox(
-      v-model="affectDefence.will",
-      @change="handleCheckDefense",
-      border) Will
+      border,
+      :key="index") {{ key.toUpperCase() }}
     el-checkbox(
       :indeterminate="isDefensesIndeterminate",
       v-model="allDefensesChecked",
       @change="handleChangeAllDefenses") {{ allDefensesChecked ? 'Uncheck all' : 'Check all'}}
 
-    el-button Cancel
-    el-button(type="success") Submit effect
+    el-button(type="text") Cancel
+    el-button(type="success", @click="submitEffect") Submit effect
 </template>
 
 <script>
@@ -98,11 +93,11 @@ export default {
   data() {
     return {
       types: EFFECT_TYPES,
-      chosen_type: 0,
+      chosenType: 0,
 
       // if save ends so we can add aftereffect and fail effect
-      fail: null,
-      aftereffect: null,
+      fail: [],
+      aftereffect: [],
 
       // if temporary we can adjust end turn
       endTurn: 0,
@@ -121,7 +116,7 @@ export default {
       ongoingDamage: 0,
       regeneration: 0,
 
-      affectDefence: {
+      affectedDefenses: {
         ac: true,
         fort: true,
         ref: true,
@@ -132,62 +127,103 @@ export default {
       defenseModifier: 0
     };
   },
+  computed: {
+    effect() {
+      return {
+        ...this.$data
+      };
+    },
+    isDefenseModifierDisabled() {
+      return !this.isDefensesIndeterminate && !this.allDefensesChecked;
+    },
+  },
   watch: {
     followingConditionsIds: "processFollowingConditions"
   },
   methods: {
     handleChangeAllDefenses(newValue) {
-      Object.keys(this.affectDefence).map(key =>
-        Vue.set(this.affectDefence, key, newValue)
+      Object.keys(this.affectedDefenses).map(key =>
+        Vue.set(this.affectedDefenses, key, newValue)
       );
       this.isDefensesIndeterminate = false;
     },
     handleCheckDefense() {
-      const defenseKeys = Object.keys(this.affectDefence);
-      const checkedCount = defenseKeys.filter(key => this.affectDefence[key])
+      const defenseKeys = Object.keys(this.affectedDefenses);
+      const checkedCount = defenseKeys.filter(key => this.affectedDefenses[key])
         .length;
       this.allDefensesChecked = checkedCount === defenseKeys.length;
       this.isDefensesIndeterminate =
         checkedCount > 0 && checkedCount < defenseKeys.length;
     },
     processFollowingConditions(conditionIds) {
-      console.log(conditionIds);
       conditionIds.forEach((conditionId) => {
         // eslint-disable-next-line consistent-return
         CONDITIONS[conditionId].chained.forEach((chainedConditionId) => {
           if (this.followingConditionsIds.indexOf(chainedConditionId) === -1) {
             // TODO if already petrified, it can to not fall prone
-            if (chainedConditionId === 12 && this.followingConditionsIds.indexOf(11) !== -1) {
+            if (
+              chainedConditionId === 12 &&
+              this.followingConditionsIds.indexOf(11) !== -1
+            ) {
               return false;
             }
             this.followingConditionsIds.push(chainedConditionId);
           }
         });
       });
+    },
+    submitEffect() {
+      console.log(this.effect);
     }
-  },
+  }
 };
 </script>
 
 <style lang="scss">
-.effect-constructor {
-  padding: 16px 32px;
+@import "../styles/element-variables.scss";
+
+$spacing: 8px;
+
+.ec {
+  overflow: hidden;
+
+  &__row {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+
+    & > * {
+      margin-right: $spacing;
+    }
+  }
+
+  &__label {
+    font-size: 12px;
+    font-weight: 500;
+    color: $--color-text-regular;
+    line-height: 14px;
+  }
+
+  &__text-input {
+    max-width: 300px;
+  }
 }
 // TODO move to separate file. or create theme.
+
 .el-checkbox-group--flex {
   display: flex;
   flex-wrap: wrap;
-  padding-top: 1px;
-}
+  margin-left: -$spacing / 2;
+  margin-right: -$spacing / 2;
 
-.el-checkbox-button--grow {
-  display: block;
-  flex-grow: 1;
-  max-width: 140px;
-  margin-top: -1px;
-  & .el-checkbox-button__inner {
-    border-left: 1px;
-    display: block;
+  & .el-checkbox {
+    flex-grow: 1;
+    margin: 0 $spacing / 2 $spacing;
+    max-width: 170px;
+  }
+
+  & .el-checkbox.is-bordered + .el-checkbox.is-bordered {
+    margin-left: $spacing / 2;
   }
 }
 </style>
