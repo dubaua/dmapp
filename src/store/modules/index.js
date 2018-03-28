229c3050b967cad9db8894df49c875fcc6342c05
dubaua <dubaua@gmail.com>
2018-03-28 10:20:44 +0500

    fix wrong destruction

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -10,7 +10,7 @@ requireModule.keys().forEach((fileName) => {
   const moduleName = camelCase(fileName.replace(/(\.\/|\.js)/g, ""));
   modules[moduleName] = {
     namespaced: true,
-    ...requireModule(fileName),
+    ...requireModule(fileName).default,
   };
 });
 
